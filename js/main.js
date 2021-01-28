ffbfb98c5e8c88cbc390933511fb5c406fa3923b
Somesh Patel <56200611+im-somax@users.noreply.github.com>
2021-01-28 10:12:48 +0530

    Updates

--- js/main.js
+++ js/main.js
@@ -52,11 +52,9 @@
 			if ( $('#ftco-nav').is(':visible') ) {
 				$(this).removeClass('active');
 			} else {
-				$(this).addClass('active');	
+				$(this).addClass('active');
 			}
 
-			
-			
 		});
 
 	};
@@ -81,7 +79,6 @@
 	};
 
 	onePageClick();
-	
 
 	var carousel = function() {
 		$('.home-slider').owlCarousel({
@@ -143,19 +140,18 @@
 
 			if (st > 150) {
 				if ( !navbar.hasClass('scrolled') ) {
-					navbar.addClass('scrolled');	
+					navbar.addClass('scrolled');
 				}
-			} 
+			}
 			if (st < 150) {
 				if ( navbar.hasClass('scrolled') ) {
 					navbar.removeClass('scrolled sleep');
 				}
-			} 
+			}
 			if ( st > 350 ) {
 				if ( !navbar.hasClass('awake') ) {
-					navbar.addClass('awake');	
+					navbar.addClass('awake');
 				}
-				
 				if(sd.length > 0) {
 					sd.addClass('sleep');
 				}
@@ -172,11 +168,7 @@
 		});
 	};
 	scrollWindow();
-
-	
-
 	var counter = function() {
-		
 		$('#section-counter, .hero-wrap, .ftco-counter, .ftco-about').waypoint( function( direction ) {
 
 			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
@@ -193,7 +185,6 @@
 					  }, 7000
 					);
 				});
-				
 			}
 
 		} , { offset: '95%' } );
@@ -207,7 +198,6 @@
 		$('.ftco-animate').waypoint( function( direction ) {
 
 			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
-				
 				i++;
 
 				$(this.element).addClass('item-animate');
@@ -229,9 +219,7 @@
 							el.removeClass('item-animate');
 						},  k * 50, 'easeInOutExpo' );
 					});
-					
 				}, 100);
-				
 			}
 
 		} , { offset: '95%' } );
@@ -273,13 +261,11 @@
   var goHere = function() {
 
 		$('.mouse-icon').on('click', function(event){
-			
 			event.preventDefault();
 
 			$('html,body').animate({
 				scrollTop: $('.goto-here').offset().top
 			}, 500, 'easeInOutExpo');
-			
 			return false;
 		});
 	};
